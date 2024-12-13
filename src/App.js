f036466628dde2fc76546df80b7219f8a3c6b715
Geegu varghese <geeguvarghese1@gmail.com>
2024-12-12 21:13:26 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './component/helloworld';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
